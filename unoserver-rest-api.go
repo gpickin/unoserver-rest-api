9f1eb92b780a51a183f5f1fb9df2052664b81393
Gavin Pickin <gavin@ortussolutions.com>
2025-02-04 11:07:08 -0800

    Change the default port to 8443

--- unoserver-rest-api.go
+++ unoserver-rest-api.go
@@ -26,7 +26,7 @@ func main() {
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "addr",
-			Value: "0.0.0.0:2004",
+			Value: "0.0.0.0:8443",
 			Usage: "The addr used by the unoserver api server",
 		},
 		cli.StringFlag{
